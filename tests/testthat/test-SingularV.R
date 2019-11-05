e9140aae83bfa251102623edf7fccdaa9ed45c66
Venelin Mitov <venelin.mitov@bsse.ethz.ch>
2019-11-05 09:46:12 +0100

    Removed dependency to ape from test (using tree from testobjects).

--- tests/testthat/test-SingularV.R
+++ tests/testthat/test-SingularV.R
@@ -1,33 +1,33 @@
-library(testthat)
-
-context("Test singular matrix V")
-
-library(PCMBase)
 library(PCMBaseCpp)
-library(ape)
 
-modelBM <- PCM(model = "BM", k = 1)
-phyltree <- rtree(5)
-mTraits <- matrix(0,ncol=5,nrow=1)
-
-metaI <- PCMInfo(mTraits, phyltree, modelBM)
-metaICpp <- PCMInfoCpp(mTraits, phyltree, modelBM)
-
-
-test_that(
-  "Singular V matrix raises a warning (PCMBase.Errors.As.Warnings=TRUE):", {
-    options(PCMBase.Errors.As.Warnings = TRUE)
-    expect_warning(loglik_PCMBase <- PCMLik(mTraits, phyltree, modelBM, metaI = metaI))
-    expect_warning(loglik_PCMBaseCpp <- PCMLik(mTraits, phyltree, modelBM, metaI = metaICpp))
-    expect_true(is.na(loglik_PCMBase) && is.na(loglik_PCMBaseCpp))
-  })
-
-test_that(
-  "Singular V matrix raises an error (PCMBase.Errors.As.Warnings=FALSE):", {
-    options(PCMBase.Errors.As.Warnings = FALSE)
-    expect_error(loglik_PCMBase <- PCMLik(mTraits, phyltree, modelBM, metaI = metaI))
-    expect_error(loglik_PCMBaseCpp <- PCMLik(mTraits, phyltree, modelBM, metaI = metaICpp))
-  })
-
-
-
+if(PCMBaseCppIsADevRelease()) {
+  library(testthat)
+  
+  context("Test singular matrix V")  
+  load("testobjects.RData")
+  
+  
+  library(PCMBase)
+  
+  modelBM <- PCM(model = "BM", k = 1)
+  
+  mTraits <- matrix(0,ncol=PCMTreeNumTips(tree.a),nrow=1)
+  
+  metaI <- PCMInfo(mTraits, tree.a, modelBM)
+  metaICpp <- PCMInfoCpp(mTraits, tree.a, modelBM)
+  
+  test_that(
+    "Singular V matrix raises a warning (PCMBase.Errors.As.Warnings=TRUE):", {
+      options(PCMBase.Errors.As.Warnings = TRUE)
+      expect_warning(loglik_PCMBase <- PCMLik(mTraits, tree.a, modelBM, metaI = metaI))
+      expect_warning(loglik_PCMBaseCpp <- PCMLik(mTraits, tree.a, modelBM, metaI = metaICpp))
+      expect_true(is.na(loglik_PCMBase) && is.na(loglik_PCMBaseCpp))
+    })
+  
+  test_that(
+    "Singular V matrix raises an error (PCMBase.Errors.As.Warnings=FALSE):", {
+      options(PCMBase.Errors.As.Warnings = FALSE)
+      expect_error(loglik_PCMBase <- PCMLik(mTraits, tree.a, modelBM, metaI = metaI))
+      expect_error(loglik_PCMBaseCpp <- PCMLik(mTraits, tree.a, modelBM, metaI = metaICpp))
+    })
+}
